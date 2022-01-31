b5a7e7ac38521dea36d7446e5b8274b0cd6ad73f
riina <54872398+riina@users.noreply.github.com>
2022-01-31 03:25:42 -0800

    Update KixManifest.cs

--- src/kix/KixManifest.cs
+++ src/kix/KixManifest.cs
@@ -4,6 +4,7 @@ namespace kix;
 
 internal record KixManifest(string BasePath, KixManifestContent Content)
 {
+    private const string DirectorySuffix = ".kix";
     private const string FileNameSuffix = ".kix.json";
     private static readonly string s_filePath = AppDomain.CurrentDomain.BaseDirectory;
     private static List<KixManifest>? s_manifests;
@@ -12,28 +13,29 @@ internal record KixManifest(string BasePath, KixManifestContent Content)
     {
         if (s_manifests != null) return s_manifests;
         s_manifests = new List<KixManifest>();
-        try
+        foreach (string dir in Directory.EnumerateDirectories(s_filePath))
         {
-            foreach (string dir in Directory.EnumerateDirectories(s_filePath))
-            {
-                foreach (string file in Directory.EnumerateFiles(dir).Where(f => f.EndsWith(FileNameSuffix, StringComparison.InvariantCultureIgnoreCase)))
-                {
-                    try
-                    {
-                        KixManifestContent content = JsonSerializer.Deserialize<KixManifestContent>(File.ReadAllText(file)) ?? throw new IOException();
-                        s_manifests.Add(new KixManifest(dir, content));
-                    }
-                    catch
-                    {
-                        // ignored
-                    }
-                }
-            }
-        }
-        catch
-        {
-            // ignored
+            s_manifests.AddRange(LoadManifestsInFolder(dir));
+            foreach (string dir2 in Directory.EnumerateDirectories(dir).Where(f => f.EndsWith(DirectorySuffix, StringComparison.InvariantCultureIgnoreCase)))
+                s_manifests.AddRange(LoadManifestsInFolder(dir2));
         }
         return s_manifests;
+    }
+
+    private static IEnumerable<KixManifest> LoadManifestsInFolder(string dir)
+    {
+        foreach (string file in Directory.EnumerateFiles(dir).Where(f => f.EndsWith(FileNameSuffix, StringComparison.InvariantCultureIgnoreCase)))
+        {
+            KixManifestContent content;
+            try
+            {
+                content = JsonSerializer.Deserialize<KixManifestContent>(File.ReadAllText(file)) ?? throw new IOException();
+            }
+            catch
+            {
+                continue;
+            }
+            yield return new KixManifest(dir, content);
+        }
     }
 }
