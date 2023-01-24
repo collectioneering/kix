8ae5dc778da270547af2d58cafc5eb9522b1637e
v69ar73aqvc <54872398+v69ar73aqvc@users.noreply.github.com>
2023-01-23 18:41:58 -0800

    Update BRun.cs

--- src/kix/BRun.cs
+++ src/kix/BRun.cs
@@ -7,6 +7,6 @@ public class BRun : IRunOutput
     [Option('v', "verbose", HelpText = "Enable verbose output.")]
     public bool Verbose { get; set; }
 
-    [Option("ignore-shared-api-version", HelpText = "Ignore Art library version validation.")]
+    //[Option("ignore-shared-api-version", HelpText = "Ignore Art library version validation.")]
     public bool IgnoreSharedAssemblyVersion { get; set; }
 }
