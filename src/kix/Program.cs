fb72520f2b14af454d598c8b97ee89117638cbe5
riina <54872398+riina@users.noreply.github.com>
2023-02-25 08:35:42 -0800

    use top level statement for entry point

--- src/kix/Program.cs
+++ src/kix/Program.cs
@@ -1,15 +1,5 @@
 ﻿using System.CommandLine;
-using System.Diagnostics.CodeAnalysis;
 using Art.Modular;
 using Art.Tesler;
 
-namespace kix;
-
-internal static class Program
-{
-    [RequiresUnreferencedCode("Loading artifact tools might require types that cannot be statically analyzed.")]
-    private static async Task<int> Main(string[] args)
-    {
-        return await TeslerRootCommand.Create(new DynamicRegistryStore()).InvokeAsync(args);
-    }
-}
+return await TeslerRootCommand.Create(new DynamicRegistryStore()).InvokeAsync(args);
