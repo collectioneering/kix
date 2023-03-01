5ebd52806650809336389535bcebf20341c51c61
riina <54872398+riina@users.noreply.github.com>
2023-03-01 12:38:42 -0800

    update art

--- src/kix/Program.cs
+++ src/kix/Program.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.Immutable;
 using System.CommandLine;
 using Art.Common;
+using Art.Common.Modular;
 using Art.Modular;
 using Art.Tesler;
 
 var cfg = new ModuleLoadConfiguration(new[] { "Art" }.ToImmutableHashSet());
 string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-var provider = ModuleManifestProvider.Create(cfg, Path.Combine(baseDir, "Plugins"), ".kix", ".kix.json");
 string? defaultPropFile = Path.Combine(baseDir, "kix_default_props.json");
 if (!File.Exists(defaultPropFile))
 {
     defaultPropFile = null;
 }
+var provider = ModuleManifestProvider.Create(cfg, Path.Combine(baseDir, "Plugins"), ".kix", ".kix.json");
+var toolLogHandlerProvider = new StyledToolLogHandlerProvider(Console.Out, Console.Error, Console.OpenStandardOutput);
+var registryStore = new ModularArtifactToolRegistryStore(provider);
 var propProvider = new DirectoryJsonDefaultPropertyProvider(baseDir, defaultPropFile);
-var console = new ErrorOnlySystemConsole();
-return await TeslerRootCommand.Create(new ModularArtifactToolRegistryStore(provider), propProvider).InvokeAsync(args, console);
+var console = new ErrorOnlyConsole(Console.Error, static () => Console.IsErrorRedirected, static () => Console.IsInputRedirected, static () => Console.WindowWidth);
+var dataProvider = new DiskTeslerDataProvider();
+var registrationProvider = new SqliteTeslerRegistrationProvider();
+var diskProfileResolver = new DiskProfileResolver();
+var selectableToolProfileResolver = new SelectableToolProfileResolver(registryStore);
+var profileResolver = new AggregateProfileResolver(new IProfileResolver[] { diskProfileResolver, selectableToolProfileResolver });
+return await TeslerRootCommand.Create(toolLogHandlerProvider, registryStore, propProvider, dataProvider, registrationProvider, profileResolver).InvokeAsync(args, console);
