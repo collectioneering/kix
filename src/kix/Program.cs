0c26b64337f59f526f00bac0c8cb54d47acecfe7
riina <54872398+riina@users.noreply.github.com>
2026-02-03 21:27:43 -0800

    update art

--- src/kix/Program.cs
+++ src/kix/Program.cs
@@ -1,31 +1,22 @@
-﻿using System.Collections.Immutable;
-using System.CommandLine;
+﻿using System.CommandLine;
+using Art;
 using Art.Common;
-using Art.Common.Modular;
 using Art.Tesler;
 using Art.Tesler.Profiles;
 using Art.Tesler.Properties;
+using Artcore;
 
-var cfg = new ModuleLoadConfiguration(new[] { "Art" }.ToImmutableHashSet());
-var providers = new List<IModuleProvider>();
+// plugins
 string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-providers.AddRange(await ModuleSearchConfigurationUtility.GetModuleProvidersByPathsAsync(
-    cfg,
-    baseDirectory,
-    Directory.GetFiles(baseDirectory, "*.kix_search_config.json")
+var registryStore = new ModularArtifactToolRegistryStore(new AggregateModuleProvider<IArtifactToolRegistry>(
+    await ModuleSearchConfigurationUtility.GetModuleProvidersByPathsAsync(
+        ModuleLoadConfiguration.Create(passthroughAssemblies: "Art"),
+        baseDirectory,
+        Directory.GetFiles(baseDirectory, "*.kix_search_config.json"),
+        Plugin.Create)
 ));
-providers.Add(ModuleManifestProvider.Create(cfg, Path.Combine(baseDirectory, "Plugins"), ".kix", ".kix.json"));
-var provider = new AggregateModuleProvider([..providers]);
-var registryStore = new ModularArtifactToolRegistryStore(provider);
-var toolLogHandlerProvider = new ConsoleStyledToolLogHandlerProvider(
-    Console.Out,
-    Console.Error,
-    Console.Error,
-    static () => Console.IsErrorRedirected,
-    static () => Console.BufferWidth,
-    static () => Console.WindowHeight,
-    static () => Console.CursorTop - Console.WindowTop,
-    Console.OpenStandardOutput);
+// logging
+var toolLogHandlerProvider = ConsoleStyledToolLogHandlerProvider.FromSystemConsole();
 // prop dirs
 const string kixDirName = ".kix";
 const string kixConfigFileName = "runnerconfig-4f648146-4fbf-0346-33aa-897b930f7b23.json";
@@ -39,23 +30,25 @@ var runnerPropProvider = new GlobalLocalRunnerPropertyProvider(globalRunnerPropP
 var globalToolPropProvider = new DirectoryJsonToolPropertyProvider(globalDir, DirectoryJsonToolPropertyProvider.DefaultFileNameTransform);
 var localToolPropProvider = new DirectoryJsonToolPropertyProvider(localDir, DirectoryJsonToolPropertyProvider.DefaultFileNameTransform);
 var toolPropProvider = new GlobalLocalToolPropertyProvider(globalToolPropProvider, localToolPropProvider);
-
+// backing
 var dataProvider = new DiskTeslerDataProvider();
 var registrationProvider = new SqliteTeslerRegistrationProvider();
 var inputRegistrationProvider = new SqliteTeslerRegistrationProvider(new Option<string>("--input-database") { HelpName = "file", Required = true, Description = "Sqlite database file (input)" });
+// profile resolution
 var diskProfileResolver = new DiskProfileResolver();
 var selectableToolProfileResolver = new SelectableToolProfileResolver(registryStore);
 var profileResolver = new AggregateProfileResolver([diskProfileResolver, selectableToolProfileResolver]);
+// command
 var rootCommand = TeslerRootCommand.Create(
-    toolLogHandlerProvider,
-    registryStore,
-    runnerPropProvider,
-    toolPropProvider,
-    dataProvider,
-    registrationProvider,
-    inputRegistrationProvider,
-    TimeProvider.System,
-    profileResolver);
+    toolLogHandlerProvider: toolLogHandlerProvider,
+    pluginStore: registryStore,
+    runnerPropertyProvider: runnerPropProvider,
+    toolPropertyProvider: toolPropProvider,
+    dataProvider: dataProvider,
+    registrationProvider: registrationProvider,
+    inputRegistrationProvider: inputRegistrationProvider,
+    timeProvider: TimeProvider.System,
+    profileResolver: profileResolver);
 var parseResult = rootCommand.Parse(args);
 parseResult.InvocationConfiguration.Output = Console.Error;
 parseResult.InvocationConfiguration.Error = Console.Error;
